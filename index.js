a3db254b955879278b13351d525809536f7fc6d6
Your Name <youremail@example.com>
2023-10-26 20:18:10 +0600

    last readable stream

--- index.js
+++ index.js
@@ -1,8 +1,6 @@
 // // for (let index = 0; index <100; index++) {
 // // console.log( index,":" ,'musaddik')
 
-
-    
 // // }
 // const fs = require('fs')
 // var data="im musaddik"
@@ -31,7 +29,7 @@
 //     if(err===null){
 //         console.log('i love you too,bracuse of err === null')
 //     }
-   
+
 // })
 // fs.appendFile("async_write.txt",'add something',(err)=>{
 //     console.log('att')
@@ -55,7 +53,6 @@
 // fs.unlink('musaddik/bio.txt',(err)=>console.log(err))
 // fs.unlink('musaddik/mybio.txt',(err)=>console.log(err))
 
-
 // -------------------------------------------os----------------------------
 
 // const os = require('os')
@@ -70,7 +67,6 @@
 // console.log(os.tmpdir())
 // console.log(os.type())
 
-
 // ----------------------path-----------------------
 
 // const path=require('path');
@@ -80,11 +76,9 @@
 
 // console.log(path.parse('D:\Back-end(23-10-2023)\NodeJs\index.js'))
 
-
 // const mypath=path.parse('D:\Back-end(23-10-2023)\NodeJs\index.js')
 // console.log(mypath.root)
 // console.log(mypath.ext)
-
 
 // ------------------opr-----------------
 // const {add,name}=require('./opr')
@@ -99,9 +93,9 @@
 // After
 // const validator=require('validator')
 // const chalk = require('chalk');
-// console.log(chalk.blue.inverse('Hello world!\n')); 
-// console.log(chalk.blue.underline('Hello world!')); 
-// console.log(chalk.blue.inverse('Hello world!\n')); 
+// console.log(chalk.blue.inverse('Hello world!\n'));
+// console.log(chalk.blue.underline('Hello world!'));
+// console.log(chalk.blue.inverse('Hello world!\n'));
 
 // const res=validator.isEmail('musaddikh13@gmail.com');
 // console.log(res,'\n')
@@ -111,14 +105,13 @@
 //     return 5+5
 // })();
 
-
 // --------------------------create server -----------------------
 
 // console.log(__dirname ,__filename)
 
 // const http=require('http');
 // const server=http.createServer((req, res) =>{
-    
+
 //     console.log(req.url)
 //     if(req.url==="/about"){
 //         res.end('this is about page ')
@@ -165,5 +158,115 @@
 // })
 
 // --------------------------------------Api(application programming interfaces )----------------------------------
-console.log('hi') 
-console.log('j')
+// const http = require("http");
+// const fs = require("fs");
+
+// // fs.readFile("myapi.json", "utf-8", (err, data) => {
+// //   if (data) {
+// //     console.log('api response');
+
+// //   } else console.log(err);
+// // });
+// const getdata=fs.readFileSync(`${__dirname}/myapi.json`, "utf-8")
+// const users = JSON.parse(getdata);
+
+// const server = http.createServer((req, res) => {
+//   console.log(req.url);
+//   if (req.url === "/about") {
+//     res.end("this is about page");
+//   } else if (req.url === "/home" || req.url === "/") {
+
+//       console.log("this is data", getdata);
+//       console.log("this is users", users);
+//       res.writeHead(200, { "Content-type": "application/json" });
+
+//     res.end(getdata);
+
+//   } else if (req.url === "/contract") {
+//     res.writeHead(200, { "Content-type": "text/html" });
+
+//        users.map((user) => res.write(`<h1>Name: ${user.name}</h1>`));
+//       res.end();
+//     res.end();
+//   } else {
+//     res.writeHead(404, { "Content-type": "text/html" });
+//     res.end('<h1 style="background: red;">this is 404</h1>');
+//   }
+// });
+// server.listen(2000, "localhost");
+
+// ----------------------------------------Events Module------------------------
+
+// console.log('it is events module')
+
+// const EventEmitter= require('events')
+// const event= new EventEmitter();
+// event.on("nameCell",()=> {
+//     console.log('i am musaddik')                  // event fire
+// })
+// event.on("nameCell",()=> {
+//     console.log('i am tazwer')                  // event fire
+// })
+// event.on("nameCell",()=> {
+//     console.log('i am zaki')                  // event fire
+// })
+// event.emit("nameCell")     // one event cell multiple function
+
+// event.on('statusCode',(status,mes)=> {
+//     console.log(`status code ${status} process : ${mes}`)
+// })
+// event.emit("statusCode",200,'ok')
+
+// -------------------------------streams and buffer----------------------
+
+// ------------without stream-----------
+// const fs = require("fs");
+// const http = require("http");
+
+// const server = http.createServer();
+
+// server.on("request", (req, res) => {
+//   fs.readFile("input.txt", "utf-8", (err, data) => {
+//     if (err) {
+//       console.log(err);
+//       res.statusCode = 500;
+//       res.end("Internal Server Error");
+//     } else {
+//       res.setHeader("Content-Type", "text/plain");
+//       res.end(data);
+//     }
+//   });
+// });
+
+// server.listen(2000, "localhost", () => {
+//   console.log("Server is running on http://localhost:2000/");
+// });
+
+
+// ----------------with stream-----------------
+
+const fs = require("fs");
+const http = require("http");
+
+const server = http.createServer();
+
+server.on("request", (req, res) => {
+  const rstream=fs.createReadStream('input.txt')
+  rstream.on('data',(getdata) => {
+    res.write(getdata)
+    res.end();
+  })
+  rstream.on('end', () =>{
+    console.log('readable stream')
+    res.end();
+  })
+  rstream.on('error', (err) =>{
+    console.log(err)
+    res.end('not found');
+
+  })
+});
+
+server.listen(2000, "localhost", () => {
+  console.log("Server is running on http://localhost:2000/");
+});
